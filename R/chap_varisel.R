15b4ab11e3bcc9eaf2995d19ef1e4c17ef169a6c
Marie-PerrotDockes <marie.perrocks@gmail.com>
2019-06-12 10:26:12 +0200

    caret

--- R/chap_varisel.R
+++ R/chap_varisel.R
@@ -1,4 +1,21 @@
-train_VariSel <- function(X, Y, type, sepx, reg, group){
+#' Title
+#'
+#' @param X
+#' @param Y
+#' @param type
+#' @param sepx
+#' @param reg
+#' @param group
+#' @param lambda
+#' @param lambda1
+#' @param lambda2
+#'
+#' @return
+#' @export
+#'
+#' @examples
+train_VariSel <- function(X, Y, type, sepx, reg, group, lambda = NULL,
+                          lambda1=NULL, lambda2 = NULL){
   if(!is.null(X) & !is.null(reg)) stop("either reg and group must be supplied or X (the design matirx already full)")
   if(!is.null(sepx) & !is.null(group)) stop("either sepx or group must be supplied see the help of the function")
   if (is.null(X)){
@@ -6,9 +23,12 @@ train_VariSel <- function(X, Y, type, sepx, reg, group){
   X <- model.matrix(~reg:group -1)
   sepx <- ":"
   }
+  if(!is.null(lambda1)){
+    lambda = list(lambda1 = lambda1, lambda2 = lambda2)
+  }
    mod <- type_to_varisel(X, Y, type,
                           Sigma_12inv = Sigma_12inv, sep= sepx)
-   mod$estime()
+   mod$estime(lambda = lambda)
 }
 
 
